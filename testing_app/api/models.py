fe5c0444f1ee45d79bbe69f482a71fbb6fc2f4d5
Vidyasagar5566 <85301020+Vidyasagar5566@users.noreply.github.com>
2023-09-23 00:23:51 +0530

    Update models.py

--- testing_app/api/models.py
+++ testing_app/api/models.py
@@ -14,12 +14,13 @@ import uuid
 class User(AbstractUser):
     email = models.CharField(default="",max_length=100,unique = True)
     username = models.CharField(default="-",max_length=100,unique = True)
+    password1 = models.CharField(default="@Vidyasag1234",max_length=100)
     roll_num = models.CharField(default="",max_length=100)
     is_sac = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
     is_faculty = models.BooleanField(default=False)
-    phn_num = models.CharField(default="+91 0000 000 000",max_length=15)
-    profile_pic = models.ImageField(upload_to = 'pics',default = 'static/img.png')
+    phn_num = models.CharField(default="+91 000 000 0000",max_length = 17)
+    profile_pic = models.ImageField(upload_to = 'uploads',default = 'static/img.png')
     file_type = models.CharField(default="0",max_length=100)
     bio = models.CharField(max_length = 400,default="@")
     sac_role = models.CharField(default="@",max_length=100)
@@ -32,6 +33,7 @@ class User(AbstractUser):
     batch = models.CharField(default="@",max_length=100)
     year = models.IntegerField(default=0)
     token = models.TextField(default="dfv",max_length=500)
+    platform = models.CharField(default="android",max_length=50)
     notif_settings = models.CharField(default="111111111",max_length=100)
     notif_seen = models.BooleanField(default=True)
     notif_count = models.IntegerField(default=0)
@@ -49,9 +51,9 @@ class Notifications(models.Model):
     title = models.CharField(max_length=100,default="")
     description = models.TextField(default="")
     branch = models.CharField(default="@",max_length=100)
-    batch = models.CharField(default="@",max_length=100)
-    year = models.CharField(default="@",max_length=100)
-    img = models.FileField(upload_to = 'pics',default = 'static/img.png')
+    batch = models.CharField(default="CS@EC@EE@ME@CE@CH@BT@AR@MT@EP@PE",max_length=100)
+    year = models.CharField(default="1111",max_length=100)
+    img = models.FileField(upload_to = 'notif',default = 'static/img.png')
     img_ratio = models.FloatField(default = 1.00)
     posted_date = models.DateTimeField(default=timezone.now)
 
@@ -65,7 +67,7 @@ class Messanger(models.Model):
     message_sender = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='message_sender')
     message_receiver = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='message_receiver')
     message_body = models.TextField(default="")
-    message_file = models.FileField(upload_to = 'pics',default = 'static/img.png')
+    message_file = models.FileField(upload_to = 'messanger',default = 'static/img.png')
     messag_file_type = models.CharField(default="@",max_length=100)
     message_body_file = models.CharField(default="@",max_length=100)
     message_replyto = models.TextField(default="@")
@@ -83,7 +85,7 @@ class CalenderEvents(models.Model):
     cal_event_type = models.CharField(max_length=100,default="self")
     title = models.CharField(max_length=100,default="")
     description = models.TextField(default="")
-    calender_date_file = models.FileField(upload_to = 'pics',default = 'static/img.png')
+    calender_date_file = models.FileField(upload_to = 'cal_events',default = 'static/img.png')
     file_type = models.CharField(default="@",max_length=100)
     branch = models.CharField(default="@",max_length=100)
     year = models.CharField(default="@",max_length=100)
@@ -91,12 +93,89 @@ class CalenderEvents(models.Model):
     posted_date = models.DateTimeField(default=timezone.now)
 
     class Meta:
-        ordering = ['-event_date']
+        ordering = ['-posted_date']
+
+    def __str__(self):
+        return str(self.title)
+
+
+
+class CalenderSub(models.Model):
+    username = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='CalenderSub_user')
+    sub_name = models.CharField(max_length=100,default="@")
+    sub_id = models.CharField(max_length=100,default="@")
+    all_years = models.TextField(default="@")
+    num_years = models.IntegerField(default=0)
+    description = models.TextField(default="@")
+    posted_date = models.DateTimeField(default=timezone.now)
+    tot_ratings_val = models.IntegerField(default=0)
+    num_ratings = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['-sub_name']
+
+    def __str__(self):
+        return str(self.sub_name)
+
+
+class CalenderSubYears(models.Model):
+    username = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='CalenderSubYears_user')
+    sub_name = models.ForeignKey(CalenderSub,on_delete=models.CASCADE, related_name='CalenderSub')
+    year_name = models.CharField(max_length=100,default="@")
+    private = models.BooleanField(default=False)
+    num_files = models.IntegerField(default=0)
+    description = models.TextField(default="@")
+    posted_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-year_name']
+
+    def __str__(self):
+        return str(self.sub_name)
+
+
+class CalenderSubFiles(models.Model):
+    username = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='CalenderSubFiles_user')
+    year_id = models.ForeignKey(CalenderSubYears, on_delete=models.CASCADE, related_name='CalenderSubYears')
+    description = models.TextField(default="@")
+    qn_ans_file = models.FileField(upload_to = 'QnsAns',default = 'static/img.png')
+    file_type = models.CharField(default="@",max_length=100)
+    file_name = models.CharField(default="@",max_length=100)
+    year = models.CharField(default="@",max_length=100)
+    posted_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-file_name']
+
+    def __str__(self):
+        return str(self.year_id)
+
+
+class Ratings(models.Model):
+    username = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='Ratings_user')
+    sub_name = models.ForeignKey(CalenderSub,on_delete=models.CASCADE, related_name='CalenderSub_ratings')
+    description = models.TextField(default="@")
+    rating = models.IntegerField(default=0)
+    verified = models.BooleanField(default=False)
+    posted_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-posted_date']
 
     def __str__(self):
         return str(self.username)
 
 
+
+
+class Reports(models.Model):
+    username = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='ReportUser')
+    description = models.TextField(default="")
+    report_belongs = models.CharField(max_length=100,default="student")
+    posted_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.username)
 
 
 
@@ -105,14 +184,14 @@ class PostTable(models.Model):
     username = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='post_table_username')
     title = models.CharField(max_length=100,default="")
     description = models.TextField(default="")
-    img = models.FileField(upload_to = 'pics',default = 'static/img.png')
+    img = models.FileField(upload_to = 'posts',default = 'static/img.png')
     img_ratio = models.FloatField(default = 1.00)
-    profile_pic = models.ImageField(upload_to = 'pics',default = 'static/img.png')
     post_file = models.IntegerField(default = 0) #vedio or audio
     tag = models.CharField(max_length = 20,default="post")   # lost_found,suggestion,problems,trending,memes/jokes,fets/club/sport,events
     is_like = models.BooleanField(default=False)
     like_count = models.IntegerField(default=0)
     comment_count = models.IntegerField(default=0)
+    post_hiders = models.TextField(default="")
     posted_date = models.DateTimeField(default=timezone.now)
     event_date = models.DateTimeField(default=timezone.now)
     Admin = models.BooleanField(default = False)
@@ -122,6 +201,10 @@ class PostTable(models.Model):
 
     def __str__(self):
          return str(self.username) + ":" + str(self.post_id)
+
+    #def delete(self):
+    #    self.filefield.delete(save=False)
+    #    super().delete()
 
 class post_Likes(models.Model):
     post_id = models.ForeignKey(PostTable, on_delete=models.CASCADE, related_name='post_like_id')
@@ -152,9 +235,11 @@ class Lost_Found(models.Model):
     username = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='lst_found_username')
     title = models.CharField(max_length=50,default="")
     description = models.TextField(default="")
-    img = models.ImageField(upload_to = 'pics',default = 'static/img.png')
+    tag = models.CharField(max_length=50,default="lost/found")
+    img = models.ImageField(upload_to = 'lost_found',default = 'static/img.png')
     img_ratio = models.FloatField(default = 1.00)
     comment_count = models.IntegerField(default=0)
+    lst_hiders = models.TextField(default="")
     posted_date = models.DateTimeField(default=timezone.now)
 
     class Meta:
@@ -162,6 +247,10 @@ class Lost_Found(models.Model):
 
     def __str__(self):
          return str(self.username) + ":" + str(self.lst_id)
+
+  #  def delete(self, *args, **kwargs):
+  #      self.img.delete(save=False)
+  #      super().delete(*args, **kwargs)
 
 class LST_Comments(models.Model):
     lst_id = models.UUIDField(default=uuid.uuid4, editable=False)
@@ -182,7 +271,7 @@ class Events(models.Model):
     username = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='events_username')
     title = models.CharField(max_length=50,default="")
     description = models.TextField(default="")
-    event_img = models.FileField(upload_to = 'pics',default = 'static/img.png')
+    event_img = models.FileField(upload_to = 'events',default = 'static/img.png')
     img_ratio = models.FloatField(default = 1.00)
     event_vedio = models.FileField(upload_to = 'pics',default = 'static/img.png')
     vedio_ratio = models.FloatField(default = 1.00)
@@ -215,9 +304,11 @@ class Alerts(models.Model):
     username = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='alerts_username')
     title = models.CharField(max_length=50,default="")
     description = models.TextField(default="")
-    img = models.FileField(upload_to = 'pics',default = 'static/img.png')
-    img_ratio = models.FloatField(default = 1.00)
+    img = models.FileField(upload_to = 'alerts',default = 'static/img.png')
+    img_ratio = models.FloatField(default = 0.0)
     comment_count = models.IntegerField(default=0)
+    allow_branchs = models.CharField(default="CS@EC@EE@ME@CE@CH@BT@AR@MT@EP@PE",max_length=100)
+    allow_years = models.CharField(default="1111",max_length=100)
     posted_date = models.DateTimeField(default=timezone.now)
 
     class Meta:
@@ -231,6 +322,10 @@ class ALERT_Comments(models.Model):
     alert_cmnt_id = models.ForeignKey(Alerts, on_delete=models.CASCADE, related_name='alert_comment')
     username = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='alert_cmnt_username',default="")
     Comment = models.TextField(default="")
+    img = models.FileField(upload_to = 'alerts',default = 'static/img.png')
+    img_ratio = models.FloatField(default = 0.0)
+    allow_branchs = models.CharField(default="CS@EC@EE@ME@CE@CH@BT@AR@MT@EP@PE",max_length=100)
+    allow_years = models.CharField(default="1111",max_length=100)
     posted_date = models.DateTimeField(default=timezone.now)
 
     class Meta:
@@ -241,7 +336,7 @@ class ALERT_Comments(models.Model):
 
 
 class Clubs_Sports(models.Model):
-    logo = models.ImageField(upload_to = 'pics',default = 'static/img.png')
+    logo = models.ImageField(upload_to = 'club_sports',default = 'static/img.png')
     title = models.CharField(max_length=50,default="")
     club_r_sport = models.CharField(max_length=50,default="")
     username = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='Club_sports_user')
@@ -250,7 +345,7 @@ class Clubs_Sports(models.Model):
     description = models.TextField(default = '')
     websites = models.CharField(max_length=100,default = '')
     sport_ground = models.TextField(default="")
-    sport_ground_img = models.ImageField(upload_to = 'pics',default = 'static/img.png')
+    sport_ground_img = models.ImageField(upload_to = 'club_sports',default = 'static/img.png')
     img_ratio = models.FloatField(default = 1.00)
     date_of_join = models.DateTimeField(default=timezone.now)
     is_like = models.BooleanField(default=False)
@@ -308,7 +403,7 @@ class Academic_table(models.Model):
 
 class Time_table(models.Model):
     branch_name = models.CharField(max_length=50,default="")
-    branch_tb_img = models.ImageField(upload_to = 'pics',default = 'static/img.png')  #
+    branch_tb_img = models.ImageField(upload_to = 'time_table',default = 'static/img.png')  #
     img_ratio = models.FloatField(default = 1.00)
     sun = models.TextField(default = '')
     mon = models.TextField(default = '')
@@ -320,8 +415,8 @@ class Time_table(models.Model):
 
     def __str__(self):
         return str(self.branch_name)
-    
- 
+
+
 
 
 
@@ -353,14 +448,14 @@ class Time_table(models.Model):
 #    e_mail = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='alert_comments_email')
 #    comment = models.CharField(default="",max_length=100)
 #    posted_date = models.DateTimeField(default=timezone.now)
-    
+
 #    class Meta:
 #        ordering = ['-posted_date']
-    
+
 #    def __str__(self):
 #        return str(self.e_mail) + ":" + str(self.alert_id)
-    
-    
+
+
 
 #class Clubs_Sports_files(models.Model):
 #    CS_file_id = models.UUIDField(default=uuid.uuid4, editable=False)
@@ -370,8 +465,8 @@ class Time_table(models.Model):
 #    title = models.CharField(max_length=50,default = '')
 #    description = models.TextField(default = '')
 #    image_vedio = models.FileField(upload_to = 'pics',default = 'static/img.png')
-    
-    
+
+
 #    class Meta:
 #        ordering = ['-posted_date']
 
