229ee0c9ade52faad7c90b59fd874739b11a3e5c
Vidyasagar5566 <85301020+Vidyasagar5566@users.noreply.github.com>
2023-09-23 00:25:08 +0530

    Update urls.py

--- testing_app/api/urls.py
+++ testing_app/api/urls.py
@@ -32,10 +32,10 @@ urlpatterns = [
  #  path('edit_notif_settings', views.EDIT_notif_settings.as_view(),name = 'EDIT_notif_settings'),
 
 
-    path('', views.testing.as_view(),name = 'login'),
-    path('login1', obtain_auth_token),
-    path('register/email_check1', views.Register_EMAIL_check.as_view(),name = 'Register1'),
-    path('get_user1', views.GET_user.as_view(),name = 'GET_user1'),
+    path('test', views.testing.as_view(),name = 'login'),
+    path('login2', obtain_auth_token),
+    path('register/email_check2', views.Register_EMAIL_check.as_view(),name = 'Register1'),
+    path('get_user2', views.GET_user.as_view(),name = 'GET_user1'),
     path('lost_found/list1', views.LST_list.as_view(),name = 'LST_list1'),
     path('lost_found/comment_list1', views.LST_Comment_list.as_view(),name = 'LST_Comment_list1'),
     path('post/list1', views.POST_list.as_view(),name = 'POST_list1'),
@@ -53,15 +53,63 @@ urlpatterns = [
     path('sac/list1', views.SAC_list.as_view(),name = 'SAC_list1'),
     path('mess/list1', views.MESS_list.as_view(),name = 'ACADEMIC_list1'),
     path('academic/list1', views.ACADEMIC_list.as_view(),name = 'ACADEMIC_list1'),
-    path('timetable/list1', views.TIMETABLE_list.as_view(),name = 'TIMETABLE_list1'),
     path('notifications1', views.Notifications.as_view(),name = 'Notifications1'),
     path('edit_notif_settings1', views.EDIT_notif_settings.as_view(),name = 'EDIT_notif_settings1'),
 
-# MESSANGER
+# MESSANGER  && NOTIFICATIONS
+    path('send_notifications1', views.SendNotifications.as_view(),name = 'send_notifications1'),
     path('messanger1', views.Messanger.as_view(),name = 'Messanger1'),
     path('user_messanger1', views.USER_Messanger.as_view(),name = 'user_messanger1'),
 #CALENDER_EVENT
-   path('calender_events1', views.CALENDER_EVENT.as_view(),name = 'CALENDER_EVENT1'),
+    path('cal_dates_subs/list1', views.CALENDER_DATE_SUBS.as_view(),name = 'CALENDER_DATE_SUBS'),   #include subjects
+    path('cal_sub_years/list1', views.CALENDER_SUB_YEARS.as_view(),name = 'CALENDER_SUB_YEARS'),
+    path('calender_sub_files1', views.CALENDER_SUB_FILES.as_view(),name = 'CALENDER_SUB_FILES'),
+    path('cal_events/list1', views.CALENDER_EVENTS_list.as_view(),name = 'CALENDER_EVENTS_list'),
+    path('timetable/list1', views.TIME_TABLE.as_view(),name = 'TIME_TABLE'),
+    path('ratings', views.RATINGS.as_view(),name = 'RATINGS'),
+
+   path('security1', views.SECURITY.as_view(),name = 'SECURITY1'),
 
 
     ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#final queryParameters = {
+#  'param1': 'one',
+#  'param2': 'two',
+#};
+#final uri =
+#    Uri.https('www.myurl.com', '/api/v1/test', queryParameters);
+#final response = await http.get(uri, headers: {
+#  HttpHeaders.authorizationHeader: 'Token $token',
+#  HttpHeaders.contentTypeHeader: 'application/json',
+#});
