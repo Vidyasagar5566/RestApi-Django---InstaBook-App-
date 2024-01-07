76e6b26922403169fcf550250fffee0665853c6b
Ubuntu <ubuntu@ip-172-31-10-142.ap-south-1.compute.internal>
2024-01-07 07:09:07 +0000

    small changes

--- testing_app/testing_app/settings.py
+++ testing_app/testing_app/settings.py
@@ -115,24 +115,24 @@ WSGI_APPLICATION = "testing_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-#DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.sqlite3",
-#        "NAME": BASE_DIR / "db.sqlite3",
-#    }
-#}
-
-
 DATABASES = {
-     "default": {
-         "ENGINE": "django.db.backends.mysql",
-         "NAME": "ESMUS",
-         "USER": "admin",
-         "PASSWORD": "1W0UVChOep1EBtxfmc2t",
-         "HOST": "esmus.c2xpl2fmhzxv.ap-southeast-1.rds.amazonaws.com",
-         "PORT": "3306",
-     }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
+
+#DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "ESMUS",
+#         "USER": "admin",
+#         "PASSWORD": "1W0UVChOep1EBtxfmc2t",
+#         "HOST": "esmus.c2xpl2fmhzxv.ap-southeast-1.rds.amazonaws.com",
+#         "PORT": "3306",
+#     }
+#}
 
 
 # Password validation
